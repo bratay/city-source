9cf19b0fb914de7e8d473a10daf14639bee64e6f
bratay <violinbt2@yahoo.com>
2020-01-22 15:19:23 -0600

    Init database

---
+++ readWrite.js
@@ -0,0 +1,18 @@
+
+src = "https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js"
+// src = "https://www.gstatic.com/firebasejs/7.7.0/firebase-analytics.js"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCGnqKQ3XqAuYctdKp49E_xOEnomZxaSQk",
+    authDomain: "city-source-8a88a.firebaseapp.com",
+    databaseURL: "https://city-source-8a88a.firebaseio.com",
+    projectId: "city-source-8a88a",
+    storageBucket: "city-source-8a88a.appspot.com",
+    messagingSenderId: "116800112457",
+    appId: "1:116800112457:web:e63f46689ea51c11437c98",
+    measurementId: "G-D07D7LCMPR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
