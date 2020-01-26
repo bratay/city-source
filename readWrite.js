20e60059369a2eaf27134d31a82c76cb8faf1932
bratay <violinbt2@yahoo.com>
2020-01-26 13:08:54 -0600

    Working test for database

--- readWrite.js
+++ readWrite.js
@@ -1,18 +1,7 @@
+document.addEventListener("DOMContentLoaded", event => {
 
-src = "https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js"
-// src = "https://www.gstatic.com/firebasejs/7.7.0/firebase-analytics.js"
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCGnqKQ3XqAuYctdKp49E_xOEnomZxaSQk",
-    authDomain: "city-source-8a88a.firebaseapp.com",
-    databaseURL: "https://city-source-8a88a.firebaseio.com",
-    projectId: "city-source-8a88a",
-    storageBucket: "city-source-8a88a.appspot.com",
-    messagingSenderId: "116800112457",
-    appId: "1:116800112457:web:e63f46689ea51c11437c98",
-    measurementId: "G-D07D7LCMPR"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    const app = firebase.app();
+    console.log(app)
+    console.log("TEST TEST \n")
+});
