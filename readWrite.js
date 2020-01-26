b282f91309f03ce8b2afd25d7e3621c5ee38559f
bratay <violinbt2@yahoo.com>
2020-01-26 14:47:53 -0600

    Configured app, analytics, and database

--- readWrite.js
+++ readWrite.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", event => {
+// Firebase App configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCGnqKQ3XqAuYctdKp49E_xOEnomZxaSQk",
+    authDomain: "city-source-8a88a.firebaseapp.com",
+    databaseURL: "https://city-source-8a88a.firebaseio.com",
+    projectId: "city-source-8a88a",
+    storageBucket: "city-source-8a88a.appspot.com",
+    messagingSenderId: "116800112457",
+    appId: "1:116800112457:web:e63f46689ea51c11437c98",
+    measurementId: "G-D07D7LCMPR"
+};
 
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+// var app = firebase.initializeApp(firebaseConfig, "City Source"); //added name
+const analytics = firebase.analytics(); //analytics object
+const db = firebase.database() //Database object 
 
-    const app = firebase.app();
-    console.log(app)
-    console.log("TEST TEST \n")
-});
+//Printing info for app, analytics, and database configs
+console.log(app, analytics,"\n", db)
