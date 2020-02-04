e2e774eb7f6214fdaf0c22c962b123aab5641eeb
Daniel Gonzalez <dgonzalez93@gmail.com>
2020-02-03 23:14:19 -0600

    Initial Map commit.

---
+++ src/Map.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+//TODO: we will need to create markers for each post type. Markers
+//      are just divs that are rendered at specific lat longs on
+//      the map. 
+/*const simpleMarker = ({ text }) => (
+    <div style={{
+      color: 'white', 
+      background: 'grey',
+      padding: '15px 10px',
+      display: 'inline-flex',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '100%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      {text}
+    </div>
+  );
+*/
+
+class CSMap extends Component {
+    //TODO: obtain initial lat long from user coords
+    static defaultProps = {
+        center: {
+            lat: 38.9543,
+            lng: -95.2558
+        },
+        zoom: 11
+    };
+    
+    render() {
+        return(
+            <div style = {{height: '92vh', width: '100%'}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBN_j754Q6zm3UK591MDk5pXDKInqW6FEA' }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+            >
+                {/* TODO: this is where we'd include markers
+                <simpleMarker
+                    lat={38.9543}
+                    lng={-95.2558}
+                    text={'KU test marker'}
+                />
+                */}
+            </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default CSMap;
