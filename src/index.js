1edf33f8aa7869787b18a0207990466208b816f0
bratay <violinbt2@yahoo.com>
2020-02-04 14:11:22 -0600

    Sign in with Google in new tab

--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import Navbar from './Navbar';
 import theme from './theme';
 import "./index.css";
+import * as firebase from 'firebase';
+
+// Firebase App configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCGnqKQ3XqAuYctdKp49E_xOEnomZxaSQk",
+	authDomain: "city-source-8a88a.firebaseapp.com",
+	databaseURL: "https://city-source-8a88a.firebaseio.com",
+	projectId: "city-source-8a88a",
+	storageBucket: "city-source-8a88a.appspot.com",
+	messagingSenderId: "116800112457",
+	appId: "1:116800112457:web:e63f46689ea51c11437c98",
+	measurementId: "G-D07D7LCMPR"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore()
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
 		<CssBaseline />
-		<Navbar login={false} current="index"/>
+		<Navbar login={false} current="index" />
 	</ThemeProvider>,
 	document.querySelector('#root')
 );
