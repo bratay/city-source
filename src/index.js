a898a88c1732e2efbb451a2758fa1e452156b58c
Daniel Gonzalez <dgonzalez93@gmail.com>
2020-02-18 15:04:26 -0600

    Updated file structure for map component. The component should be converted to a react hook at a later date.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CitySourceContainer from './CitySourceContainer.jsx';
 import theme from './theme';
 import "./index.css";
 import Navbar from "./Navbar.js"
-import CSMap from "./Map.js"
+import CSMap from "./components/MapUI/Map.js"
 import * as firebase from 'firebase';
 
 // Firebase App configuration
