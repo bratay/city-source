55b1d63530a14086705936cc17b0579ffb1c8744
Sydney <sydneyycombs@gmail.com>
2020-02-25 14:04:09 -0600

    --

--- src/apiKey.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Firebase_API_Key = 'd';
-
-export const Google_Maps_API_Key = 'd';
--- src/signIn.js
+++ src/signIn.js
@@ -1,13 +1,15 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import { db } from './index.js';
-import { getUserProfileObj } from "./profileBackEnd"
+import { getHometownCoor } from './components/MapUI/Map.js';
 
-export var googleUserObj;
+export var user;
 var userToken;
 
 export var currentUserObj = {
     bio: "",
     hometown: "",
+    hometownCoor: new firebase.firestore.GeoPoint( 0, 0),
+    email: "",
     picUrl: "",
     userID: 0,
     username: "",
@@ -19,49 +21,57 @@ export function googleSignIn() {
     var provider = new firebase.auth.GoogleAuthProvider(); //Google sign in object
 
     firebase.auth().signInWithRedirect(provider).then(function (result) {
-        // save Google user object
-        googleUserObj = result
-        userToken = googleUserObj.credential.accessToken
+        //Save user object
     }).catch(function (error) {
         console.log('Sign in fail! Error message: ', error.message)
         return -1
     });
+
+    user = firebase.auth().currentUser
+    userToken = String(user.uid)  // Use User.getToken() instead. Need to fix this in the future
 
     if (!userExist(userToken)) {
         // Creates new document with token as name of doc
         db.collection('users').doc(userToken).set({
             bio: "",
             hometown: "",
-            picUrl: googleUserObj.picUrl,
-            userID: userToken,
-            username: googleUserObj.displayName,
+            hometownCoor: new firebase.firestore.GeoPoint.init( 0, 0),
+            email: user.email,
+            picUrl: String(user.photoURL),
+            userID: user.uid,
+            username: user.displayName,
             userType: 0
         })
 
         autoUpdateUserObject(db.collection('user').doc(userToken))
-
-        var temp = userToken
-        testing(temp)
-
         return 0
     } else {
-
         autoUpdateUserObject(db.collection('user').doc(userToken))
-
-        var temp = userToken
-        testing(temp)
-
         return 1
     }
 }
 
-function testing(token) {
-    console.log(getUserProfileObj(token))
+//signs out current user
+export function signOut() {
+    firebase.auth().signOut()
+
+    currentUserObj = {
+        bio: "",
+        hometown: "",
+        hometownCoor: new firebase.firestore.GeoPoint.init( 0, 0),
+        email: "",
+        picUrl: "",
+        userID: 0,
+        username: "",
+        userType: 0
+    }
 }
 
 export function saveHometown(hometown) {
-    db.collection('users').doc(googleUserObj.credential.accessToken).update({
-        hometown: hometown
+    var homeTownGeoPoint = getHometownCoor(hometown)
+    db.collection('users').doc(user.credential.accessToken).update({
+        hometown: hometown,
+        hometownCoor: homeTownGeoPoint
     }).catch(function (error) {
         // The document probably doesn't exist.
         console.error("Error updating document: ", error);
@@ -77,6 +87,8 @@ function autoUpdateUserObject(currentUser) {
         currentUserObj = {
             bio: doc.bio,
             hometown: doc.hometown,
+            hometownCoor: new firebase.firestore.GeoPoint.init( 0, 0),
+            email: doc.email,
             picUrl: doc.picUrl,
             userID: doc.userID,
             username: doc.username,
@@ -85,7 +97,7 @@ function autoUpdateUserObject(currentUser) {
     });
 }
 
-// example of ID check func
+// example of ID check function
 export function userExist(token) {
     // return (db.collection('users').doc(token) != null) ? true : false
     return false;
