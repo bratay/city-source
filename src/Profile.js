fcb84d0651db9bad77d38e9b31c7e45eeade70a7
Jacob Parnell <jparnell1997@gmail.com>
2020-02-25 12:53:45 -0600

    Added layout for profile edit dialog

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Avatar, Button, Dialog, DialogContentText, DialogTitle, DialogContent, Grid, Slide, Typography, Container, Tooltip, IconButton, Divider, Card } from '@material-ui/core';
+import { Avatar, Button, Card, Container, Dialog, DialogActions, DialogContent, DialogContentText, Divider, Grid, IconButton, Slide, Snackbar, TextField, Tooltip, Typography, InputBase } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import MuiAlert from '@material-ui/lab/Alert';
+import { getUserProfileObj } from "./profileBackEnd.js";
 import CloseIcon from '@material-ui/icons/Close';
 import RoomIcon from '@material-ui/icons/Room';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 
 const useStyles = makeStyles(theme => ({
 	avatar: {
@@ -50,15 +52,14 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 	return <Slide direction="up" ref={ref} {...props} />;
 });
 
-// function ProfileModal(props) {
-	
+function Alert(props) {
+	return (
+		<MuiAlert elevation={6} variant="filled" {...props} />
+	);
+}
 
-// 	return (
-
-// 	);
-// }
-
-function ProfileTest(props) {
+export function ProfileEdit(props) {
+	const userId = props.userId;
 	const classes = useStyles();
 	const [open, setOpen] = React.useState(false);
 
@@ -68,6 +69,80 @@ function ProfileTest(props) {
 	const handleClose = () => {
 		setOpen(false);
 	}
+
+	const saveChanges = () => {
+		modified = false;
+	}
+
+	const onModify = () => {
+		modified = true;
+	}
+
+	let profileObj = getUserProfileObj(userId);
+
+	let modified = false;
+
+	return (
+		<React.Fragment>
+			<Button variant="contained" color="secondary" onClick={handleClickOpen}>Profile Edit Test</Button>
+			<Dialog open={open} onClose={handleClose} TransitionComponent={Transition} keepMounted fullWidth={true} maxWidth={'md'} scroll={'body'}>
+			<IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+          		<CloseIcon />
+        	</IconButton>
+				<DialogContent>
+					<DialogContentText>
+						<Container>
+							<Grid container spacing={2} alignItems="flex-end" className={classes.userBasics}>
+								<Grid item>
+									<div className ={classes.avatarParent}>
+										<Avatar className={classes.avatar} />
+										<VerifiedUserIcon color="secondary" fontSize="large" className={classes.devIcon} />
+									</div>
+								</Grid>
+								<Grid item sm={12} md container>
+									<Grid item sm={12} container direction="column" spacing={2} justify="flex-end">
+										<Grid item xs>
+											<TextField id="name" label="Name" fullWidth onChange={onModify} />
+											<Grid container spacing={1} alignItems="flex-end">
+												<Grid item>
+													<RoomIcon />
+												</Grid>
+												<Grid item>
+													<TextField id="input-with-icon-grid" label="Hometown" fullWidth onChange={onModify} />
+												</Grid>
+											</Grid>
+										</Grid>
+									</Grid>
+								</Grid>
+							</Grid>
+							<Divider style={{ marginBottom: "1em" }}/>
+							<TextField id="bio" label="Bio" multiline fullWidth rows="4" variant="outlined" onChange={onModify} />
+						</Container>
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={saveChanges} color="primary">
+            			Save Changes
+          			</Button>
+				</DialogActions>
+			</Dialog>
+		</React.Fragment>
+	);
+}
+
+export function ProfileDialog(props) {
+	const userId = props.userId;
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+	}
+
+	let profileObj = getUserProfileObj(userId);
 
 	return (
 		<React.Fragment>
@@ -116,5 +191,3 @@ function ProfileTest(props) {
 		</React.Fragment>
 	);
 }
-
-export default ProfileTest;
