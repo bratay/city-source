fd4e66220029494bbd98f7801d4401242b999e67
bratay <violinbt2@yahoo.com>
2020-03-28 18:40:17 -0500

    Comment creation

--- src/postBackEnd.js
+++ src/postBackEnd.js
@@ -2,12 +2,12 @@ import { db } from './index.js';
 import { currentUserObj, userExist } from './signIn.js';
 
 //Returns a list of comments obj in chronological order with a local or non-local flag
-export async function getCommentsFromPost(postID){
+export async function getCommentsFromPost(postID) {
     var commentsObjList = []
     var postComments = db.collection('comments').where('postID', '==', postID)
 
     await postComments.get().then(allCommments => {
-        allCommments.forEach( comment => {
+        allCommments.forEach(comment => {
             comment = createCommentObj(comment)
             commentsObjList.push(comment)
         })
@@ -18,7 +18,7 @@ export async function getCommentsFromPost(postID){
     return commentsObjList
 }
 
-function createCommentObj(commentDoc){
+function createCommentObj(commentDoc) {
     let commentObj = {}
 
     commentObj = {
@@ -34,4 +34,38 @@ function createCommentObj(commentDoc){
     }
 
     return commentObj
+}
+
+export async function createComment(commentString, postID) {
+    let commentID = Math.random().toString(36).substr(2, 9) //Generate unique comment ID
+    let isLocalComment = isLocal()
+    let newComment = {}
+
+    //Making sure Generated ID is unique 
+    while (db.collection('comment').doc(commentID) == undefined)
+        commentID = Math.random().toString(36).substr(2, 9)
+
+    newComment = {
+        comment = commentString,
+        userID = currentUserObj.userID,
+        postID = postID,
+        commentID = commentID,
+        likes = 0,
+        dislikes = 0,
+        reported = false,
+        timestamp = 0,//TODO
+        local = isLocalComment
+    }
+
+    db.collection('comment').doc(commentID).set({
+        newComment
+    })
+
+    return newComment
+}
+
+function isLocal() {
+    let local = true
+    //TODO
+    return local
 }
